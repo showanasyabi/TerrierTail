000021693d5edb720678cb1fecf3adccdfec746d
Esmail Asyabi <easyabi@bu.edu>
2017-12-03 19:28:47 -0500

    Update replace.sh

--- hypercall/dom0/replace.sh
+++ hypercall/dom0/replace.sh
@@ -1,1 +1,10 @@
 
+#!/bin/bash
+
+
+LINUX_ROOT=$1
+
+cat ./netback.c > $LINUX_ROOT/drivers/net/xen-netback/netback.c
+cat ./events_base.c > $LINUX_ROOT/drivers/xen/events/events_base.c
+cat ./events.h > $LINUX_ROOT/include/xen/events.h
+cat ./event_channel.h > $LINUX_ROOT/include/xen/interface/event_channel.h
