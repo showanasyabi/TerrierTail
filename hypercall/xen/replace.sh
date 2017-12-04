57dbf8731dfd9c7ebadc036c57d4ec62132ecef0
Esmail Asyabi <easyabi@bu.edu>
2017-12-03 19:32:22 -0500

    Update replace.sh

--- hypercall/xen/replace.sh
+++ hypercall/xen/replace.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+XEN_ROOT=$1
+
+cat xlat.lst > $XEN_ROOT/xen/include/xlat.lst
+cat xlat.c > $XEN_ROOT/xen/common/compat/xlat.c
+cat sched.h > $XEN_ROOT/xen/include/sched.h
+cat event_channel.h > $XEN_ROOT/xen/include/public/event_channel.h
+cat event_channel.c > $XEN_ROOT/xen/common/event_channel.c
+cat event.h > $XEN_ROOT/xen/include/xen/event.h
+
